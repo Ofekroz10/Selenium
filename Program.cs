a5e61dc00f5300eb52bf4b49399940c2899fd3aa
Ofekroz10 <ofekrozenkrantz@gmail.com>
2020-03-08 01:49:13 +0200

    my pom example

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        /* IWebDriver- driver to create a reference to any browser:
+        ChromeDriver, FirefoxDriver... */
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        [SetUp] //Called before each test
+        public void Init()
+        {
+            PropertiesCollection.driver = new ChromeDriver();
+            /* Navigate to google page, Navigate function return INavigation, that have the function GoToUrl */
+            PropertiesCollection.driver.Navigate().GoToUrl("http://executeautomation.com/demosite/Login.html");
+            Console.WriteLine("Called SetUp");
+        }
+
+        [Test]
+        public void ExcuteTest()
+        {
+            //Init prop
+            LoginPageObject login_page = new LoginPageObject();
+            SecondPage userPage = login_page.Login("Ofek", "12345");
+            userPage.FillUserForm("initial", "ofek-middle", "Ofek");
+
+        }
+
+
+        [TearDown] 
+        public void Clean()
+        {
+            Console.WriteLine("Close");
+            PropertiesCollection.driver.Close();
+        }
+    }
+}
---
+++ SecondPage.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class SecondPage
+    {
+        public SecondPage()
+        {
+            PageFactory.InitElements(PropertiesCollection.driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "TitleId")]
+        public IWebElement TitleId { get; set; }
+
+        [FindsBy(How = How.Id, Using = "FirstName")]
+        public IWebElement FirstName { get; set; }
+
+        [FindsBy(How = How.Name, Using = "Save")]
+        public IWebElement Save_Button { get; set; }
+
+        [FindsBy(How = How.Id, Using = "Initial")]
+        public IWebElement Initial { get; set; }
+
+        [FindsBy(How = How.Id, Using = "MiddleName")]
+        public IWebElement MiddleName { get; set; }
+
+
+        public void FillUserForm(string initial, string middleName, string firstName)
+        {
+            this.MiddleName.SendKeys(middleName);
+            this.Initial.SendKeys(initial);
+            this.FirstName.SendKeys(firstName);
+            this.Save_Button.Click();
+        }
+
+    }
+}
